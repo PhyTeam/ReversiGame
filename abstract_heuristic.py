ad4ede3b4e640e2078f545f78ba7d961e3c9e1b8
OKATA <ngothivietbao@gmail.com>
2016-05-11 09:51:45 +0700

    Update something

--- abstract_heuristic.py
+++ abstract_heuristic.py
@@ -1,5 +1,5 @@
 from node import *
-
+from heuristicB import *
 
 class AbstractHeuristic:
     def __init__(self):
@@ -25,3 +25,28 @@ class DummyHeuristic(AbstractHeuristic):
 
         enemy_index = Node.PLAYER_2
         return node.get_score(my_index) - node.get_score(enemy_index)
+
+class BestHeuristic(AbstractHeuristic):
+    def eval(self, node):
+        evalHeu = heuristicB(node)
+        my_index = Node.PLAYER_1
+        enemy_index = Node.PLAYER_2
+
+        # normalize
+        good_1 = float(node.get_score(my_index))
+        good_2 = float(node.get_score(enemy_index))
+        if good_1 == 0 and good_2 == 0:
+            return 0
+        elif good_2 == 0:
+            return 1000
+        elif good_1 == 0:
+            return -1000
+        strong_1 = float(evalHeu.get_value_of_men(node, my_index) / good_1)
+        strong_2 = float(evalHeu.get_value_of_men(node, enemy_index) / good_2)
+        reached = good_1 + good_2
+        if strong_1 + strong_2 == 0 or good_1 + good_2 == 0:
+            return 0
+        # return (strong_1-strong_2) / (strong_1 + strong_2) * (64 - reached) + (good_1 - good_2)/(good_1 + good_2) * reached
+        return strong_1 - strong_2
+        # return  strong_1/good_1 - strong_2/good_2
+        # return node.get_score(my_index) - node.get_score(enemy_index)
