0b449a92c7f5b2dde7ec1c9f7b86376235c0da79
PhyTeam <buibaphuc95@gmail.com>
2016-05-13 00:35:36 +0700

    Fixed connect new server

--- reversi_client.py
+++ reversi_client.py
@@ -51,7 +51,8 @@ def play(handler):
     global play_reversi
     play_reversi = handler
     # Use socketio with defined Namespace
-    socketIO = SocketIO('localhost', 8100, ReversiNamespace, params={'token': token})
+    socketIO = SocketIO('localhost', 8100, params={'token': token})
+    gameplay = socketIO.define(ReversiNamespace, '/play')
 
     socketIO.wait()
 
