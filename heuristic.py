48531e510661fbfb0bf10e30fcbd4d21782512d5
NhatNam <nhatnamtamtra@gmail.com>
2016-05-13 11:21:01 +0700

    test heuristic link https://github.com/kartikkukreja/blog-codes/blob/master/src/Heuristic%20Function%20for%20Reversi%20(Othello).cpp

--- heuristic.py
+++ heuristic.py
@@ -1,5 +1,5 @@
 from abstract_heuristic import *
-
+from node_advance import popcount
 
 class heuristic(AbstractHeuristic):
 
@@ -360,3 +360,104 @@ class HeuristicAdvance(AbstractHeuristic):
             return self.eval_mid_game(node)
         else:
             return self.eval_late_game(node)
+
+X1 = [-1, -1, 0, 1, 1, 1, 0, -1]
+Y1 = [0, 1, 1, 1, 0, -1, -1, -1]
+v20 = 0x8100000000000081
+v_3 = 0x4281001818008142
+v11 = 0x2400810000810024
+v8 = 0x1800008181000018
+v_7 = 0x0042000000004200
+v_4 = 0x0024420000422400
+v1 = 0x0018004242001800
+v2 = 0x00003C24243C0000
+
+
+class HeuristicNam(AbstractHeuristic):
+    def eval(self, node):
+        """"""
+        p20 = popcount(v20 & node.bitboard[1])
+        p_3 = popcount(v_3 & node.bitboard[1])
+        p11 = popcount(v11 & node.bitboard[1])
+        p8 = popcount(v8 & node.bitboard[1])
+        p_7 = popcount(v_7 & node.bitboard[1])
+        p_4 = popcount(v_4 & node.bitboard[1])
+        p1 = popcount(v1 & node.bitboard[1])
+        p2 = popcount(v2 & node.bitboard[1])
+
+        o20 = popcount(v20 & node.bitboard[-1])
+        o_3 = popcount(v_3 & node.bitboard[-1])
+        o11 = popcount(v11 & node.bitboard[-1])
+        o8 = popcount(v8 & node.bitboard[-1])
+        o_7 = popcount(v_7 & node.bitboard[-1])
+        o_4 = popcount(v_4 & node.bitboard[-1])
+        o1 = popcount(v1 & node.bitboard[-1])
+        o2 = popcount(v2 & node.bitboard[-1])
+        
+        d = 20*(p20 - o20) - 3*(p_3 - o_3) + 11*(p11 - o11) + 8*(p8 - o8) -7*(p_7 - o_7) -4*(p_4 - o_4) + p1 - o1 + 2*(p2 - o2)
+
+        my_f_tiles = 0
+        opp_f_tiles = 0
+
+        for i in xrange(8):
+            for j in xrange(8):
+                if node.get_at(i, j) != 0:
+                    for k in xrange(8):
+                        x = i + X1[k]
+                        y = j + Y1[k]
+                        if (x >= 0) and (x < 8) and (y >= 0) and (y < 8) and node.get_at(x, y) == 0:
+                            if node.get_at(i, j) == 1:
+                                my_f_tiles += 1
+                            else:
+                                opp_f_tiles += 1
+                            break
+        my_tiles = node.get_score(1)
+        opp_tiles = node.get_score(-1)
+
+        p = 0
+
+        if my_tiles > opp_tiles:
+            p = my_tiles/(my_tiles + opp_tiles)
+        elif my_tiles < opp_tiles:
+            p = -opp_tiles / (my_tiles + opp_tiles)
+
+        f = 0
+
+        if my_f_tiles > opp_f_tiles:
+            f = -my_f_tiles/(my_f_tiles + opp_f_tiles)
+        elif my_f_tiles < opp_f_tiles:
+            f = opp_f_tiles/(my_f_tiles + opp_f_tiles)
+
+        # Corner occupancy
+        c = 0
+        c += node.get_at(0, 0) + node.get_at(0, 7) + node.get_at(7, 0) + node.get_at(7, 7)
+
+        # Corner closeness
+        l = 0
+        le_up = 0x40C0000000000000
+        ri_up = 0x0203000000000000
+        le_do = 0x000000000000C040
+        ri_do = 0x0000000000000302
+        if node.get_at(0, 0) == 0:
+            l += popcount(node.bitboard[1] & le_up) - popcount(node.bitboard[-1] & le_up)
+        if node.get_at(0, 7) == 0:
+            l += popcount(node.bitboard[1] & ri_up) - popcount(node.bitboard[-1] & ri_up)
+        if node.get_at(7, 0) == 0:
+            l += popcount(node.bitboard[1] & le_do) - popcount(node.bitboard[-1] & le_do)
+        if node.get_at(7, 7) == 0:
+            l += popcount(node.bitboard[1] & ri_do) - popcount(node.bitboard[-1] & ri_do)
+
+        # Mobility
+        my_mo = node.get_mobility(1) 
+        opp_mo = node.get_mobility(-1)
+        m = 0
+        if my_mo > opp_mo:
+            m = my_mo / (my_mo + opp_mo)
+        elif my_mo < opp_mo:
+            m = -opp_mo / (my_mo + opp_mo)
+        
+
+        # final weight
+
+        score = (1000 * p) + (20043.1 * c) - 4775.325 * l + (7892.2 * m) + (7439.6 * f) + (10 * d)
+        return score
